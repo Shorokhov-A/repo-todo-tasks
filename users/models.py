0cd3f44ff099ff8add23490edd6254e3cc12c4a9
Shorockhov-A <andr2409@bk.ru>
2021-12-10 00:39:47 +0300

    edited User model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    user_name = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.CharField(max_length=64, blank=True, null=True, unique=True)
